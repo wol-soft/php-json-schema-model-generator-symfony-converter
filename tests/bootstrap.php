1842cc80e89d00594bdae740a0972b0826681fcb
Enno Woortmann <enno.woortmann@web.de>
2020-11-17 11:42:59 +0100

    Update dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,10 @@
 
 use PHPModelGenerator\Model\GeneratorConfiguration;
 use PHPModelGenerator\ModelGenerator;
+use PHPModelGenerator\SchemaProvider\RecursiveDirectoryProvider;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $generator = new ModelGenerator((new GeneratorConfiguration())->setNamespacePrefix('\\App\\Model'));
 $generator->generateModelDirectory(__DIR__ . '/App/Model');
-$generator->generateModels(__DIR__ . '/App/Schema', __DIR__ . '/App/Model');
+$generator->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/App/Schema'), __DIR__ . '/App/Model');
